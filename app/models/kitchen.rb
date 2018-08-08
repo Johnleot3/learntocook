a29014745e036c8fe51ea465569ee9cb731311fc
John Tarver <john@atriptyc.com>
2018-08-08 16:54:17 +0100

    made more cards

--- app/models/kitchen.rb
+++ app/models/kitchen.rb
@@ -2,7 +2,7 @@ class Kitchen < ApplicationRecord
   belongs_to :user
   # has_many :bookings
 
-  validates :name, presence: true, uniqueness: true
-  validates :location, presence: true, uniqueness: true
+  validates :name, presence: true
+  validates :location, presence: true
   validates :capacity, presence: true
 end

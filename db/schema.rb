83b8c89d2154d642c0affab2f8f849784842f38b
John Tarver <john@atriptyc.com>
2018-08-10 12:55:49 +0100

    image change kitchen

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_08_09_105758) do
+ActiveRecord::Schema.define(version: 2018_08_10_115446) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -36,6 +36,7 @@ ActiveRecord::Schema.define(version: 2018_08_09_105758) do
     t.text "description"
     t.string "languages", default: [], array: true
     t.integer "price"
+    t.string "kitchen_pic"
     t.index ["user_id"], name: "index_kitchens_on_user_id"
   end
 
